632caae3a3863073f0b6244b036c03eb89a67abb
David Mutabazi <davidmutabazi@outlook.com>
2024-01-18 18:05:08 -0600

    redesign process header

--- app/components/mainPage/CompaniesSection.tsx
+++ app/components/mainPage/CompaniesSection.tsx
@@ -3,20 +3,25 @@ import GoogleLogo from "../../../public/images/google.png";
 import AirbnbLogo from "../../../public/images/abnb.png";
 import MsftLogo from "../../../public/images/msft.png";
 import SlackLogo from "../../../public/images/slack.png";
-import { Container, Flex } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import Image from "next/image";
 
 const CompaniesSection = () => {
   return (
     <Container className="px-3 lg:px-0">
       <div >
-        <div className="text-center text-xl pb-6 pt-2">
-          <p className="inline">Trusted by big and small</p>
-          <p className="text-orange-400 inline font-bold"> Companies</p>
-        </div>
+        <Box className="relative text-center py-5">
+          <Box className="uppercase font-bold text-7xl lg:text-8xl opacity-10">
+            Trusted
+          </Box>
+          <Box className=" max-w-[30rem] lg:max-w-none leading-[1.8rem] capitalize font-bold text-3xl lg:text-4xl absolute top-[1.8rem] lg:top-[2.8rem] left-[1rem] lg:left-[18rem] text-center">
+            <p className="inline text-[#004b4b]">Trusted by big and small</p>
+            <p className="text-orange-400 inline font-bold"> Companies</p>
+          </Box>
+        </Box>
 
         <Flex align="center" gap="2" justify="between" className="pb-6">
-          <Image src={GoogleLogo} alt="google logo" height="80" width="80" />
+          <Image src={GoogleLogo} alt="google logo" height="40" width="40" />
           <Image src={MsftLogo} alt="Microsoft logo" height="110" width="110" />
           <Image src={SlackLogo} alt="Slack logo" height="80" width="80" />
           <Image src={AirbnbLogo} alt="Airbnb logo" height="80" width="80" />
