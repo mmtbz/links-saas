c90ad7c821c6c27223cb229e34523663e7b19457
David Mutabazi <davidmutabazi@outlook.com>
2024-01-30 17:34:17 -0600

    add tasks mock

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -15,7 +15,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { CiLogout } from "react-icons/ci";
 import { FaUserAlt } from "react-icons/fa";
-import { IoMdSettings } from "react-icons/io";
+import { GoPlus } from "react-icons/go";
+import { IoIosMore, IoMdSettings } from "react-icons/io";
 import { IoStar } from "react-icons/io5";
 
 const TasksPage = () => {
@@ -39,7 +40,7 @@ const TasksPage = () => {
       </Box>
       <Grid
         columns={{ sm: "1", lg: "4" }}
-        className=" grid-cols-1 lg:grid-cols-4 py-4"
+        className="grid-cols-1 lg:grid-cols-4 py-4 px-3 lg:px-0"
         gap="4"
       >
         <Box className=" col-span-1 shadow-lg border border-solid border-[#E4E5E7] rounded-[5px] hidden lg:block">
@@ -167,7 +168,132 @@ const TasksPage = () => {
             </Box>
           </Box>
         </Box>
-        <Box className="col-span-1 lg:col-span-3"></Box>
+        <Box className="col-span-1 lg:col-span-3">
+          <Flex align="center" justify="between">
+            <Text className="font-semibold text-lg">Board</Text>
+            <select className="text-sm p-1">
+              <option>This week</option>
+              <option>This month</option>
+            </select>
+          </Flex>
+          <Flex gap="2" className="flex-col lg:flex-row">
+            <Box className="flex-1 p-2 bg-[#EEF2F5] rounded-[10px]">
+              <Flex justify="between" align="center">
+                <Text className="text-sm">To do</Text>
+                <Flex gap="2">
+                  <GoPlus />
+                  <IoIosMore />
+                </Flex>
+              </Flex>
+              <Flex
+                className="bg-white rounded-[10px] p-2 my-2"
+                direction="column"
+                gap="2"
+              >
+                <Flex justify="between" align="center">
+                  <Flex align="center" gap="2">
+                    <Box className="bg-red-500 h-2 w-2 rounded-full"></Box>
+                    <Text className="text-xs uppercase">Mobile Dev</Text>
+                  </Flex>
+                  <IoIosMore />
+                </Flex>
+                <Text className="font-semibold block">Figma Design - V1</Text>
+                <Text className="text-sm leading-[0.25rem]">
+                  Create a design system for a hero section in 2 different
+                  variants. Create a simple presentation with these components.
+                </Text>
+                <Box>
+                  <Avatar fallback="DM" size="2" radius="full" color="green" />
+                </Box>
+              </Flex>
+            </Box>
+            <Box className="flex-1 p-2 bg-[#EEF2F5] rounded-[10px]">
+              <Flex justify="between" align="center">
+                <Text className="text-sm">In Progress</Text>
+                <Flex gap="2">
+                  <GoPlus />
+                  <IoIosMore />
+                </Flex>
+              </Flex>
+              <Flex
+                className="bg-white rounded-[10px] p-2 my-2"
+                direction="column"
+                gap="2"
+              >
+                <Flex justify="between" align="center">
+                  <Flex align="center" gap="2">
+                    <Box className="bg-green-500 h-2 w-2 rounded-full"></Box>
+                    <Text className="text-xs uppercase">Mobile Dev</Text>
+                  </Flex>
+                  <IoIosMore />
+                </Flex>
+                <Text className="font-semibold block">Figma Design - V1</Text>
+                <Text className="text-sm leading-[0.25rem]">
+                  Create a design system for a hero section in 2 different
+                  variants. Create a simple presentation with these components.
+                </Text>
+                <Box>
+                  <Avatar fallback="DM" size="2" radius="full" color="blue" />
+                </Box>
+              </Flex>
+              <Flex
+                className="bg-white rounded-[10px] p-2 my-2"
+                direction="column"
+                gap="2"
+              >
+                <Flex justify="between" align="center">
+                  <Flex align="center" gap="2">
+                    <Box className="bg-green-500 h-2 w-2 rounded-full"></Box>
+                    <Text className="text-xs uppercase">Mobile Dev</Text>
+                  </Flex>
+                  <IoIosMore />
+                </Flex>
+                <Text className="font-semibold block">Figma Design - V1</Text>
+                <Text className="text-sm leading-[0.25rem]">
+                  Create a design system for a hero section in 2 different
+                  variants. Create a simple presentation with these components.
+                </Text>
+                <Box>
+                  <Avatar fallback="DM" size="2" radius="full" color="blue" />
+                </Box>
+              </Flex>
+            </Box>
+            <Box className="flex-1 p-2 bg-[#EEF2F5] rounded-[10px]">
+              <Flex justify="between" align="center">
+                <Text className="text-sm">Done</Text>
+                <Flex gap="2">
+                  <GoPlus />
+                  <IoIosMore />
+                </Flex>
+              </Flex>
+              <Flex
+                className="bg-white rounded-[10px] p-2 my-2"
+                direction="column"
+                gap="2"
+              >
+                <Flex justify="between" align="center">
+                  <Flex align="center" gap="2">
+                    <Box className="bg-yellow-500 h-2 w-2 rounded-full"></Box>
+                    <Text className="text-xs uppercase">
+                      Backend | Sporty APP
+                    </Text>
+                  </Flex>
+                  <IoIosMore />
+                </Flex>
+                <Text className="font-semibold block">
+                  Database design - MySQL
+                </Text>
+                <Text className="text-sm leading-[0.25rem]">
+                  Create a design system for a hero section in 2 different
+                  variants. Create a simple presentation with these components.
+                </Text>
+                <Box>
+                  <Avatar fallback="DM" size="2" radius="full" color="red" />
+                </Box>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
       </Grid>
     </Container>
   );
