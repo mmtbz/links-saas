b404e0a6216aa36dc64f6af1b652817bcff1ffad
David Mutabazi <davidmutabazi@outlook.com>
2024-02-26 22:14:54 -0600

    implement client side validation

--- app/SchemaValidation.ts
+++ app/SchemaValidation.ts
@@ -21,3 +21,9 @@ export const patchUserSchema = z.object({
     .optional()
     .nullable(),
 });
+
+
+export const createTaskSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required"),
+});
--- app/dashboard/tasks/new/page.tsx
+++ app/dashboard/tasks/new/page.tsx
@@ -1,29 +1,52 @@
 "use client";
 import Title from "@/app/components/Title";
-import { Flex, Text, Box } from "@radix-ui/themes";
+import { Flex, Text, Box, Callout } from "@radix-ui/themes";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createTaskSchema } from "@/app/SchemaValidation";
+import { z } from "zod";
+import { useState } from "react";
+import { IoMdInformationCircleOutline } from "react-icons/io";
 
-interface NewTaskForm {
-  title: string;
-  description: string;
-}
+type NewTaskForm = z.infer<typeof createTaskSchema>;
 
 const CreateTaskPage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<NewTaskForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewTaskForm>({
+    resolver: zodResolver(createTaskSchema),
+  });
+
+  const [error, setError] = useState("");
   return (
     <Box>
       <Title title="Create a new task" />
+      {error && (
+        <Callout.Root color="red" className="mb-3">
+          <Callout.Icon>
+            <IoMdInformationCircleOutline />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
       <form
         className=" space-y-3"
-        onSubmit={handleSubmit(
-          async (data) => {await axios.post("/api/task", data)
-        router.push("/dashboard/tasks")}
-        )}
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/task", data);
+            router.push("/dashboard/tasks");
+          } catch (error) {
+            setError("Something happened, please try again later");
+          }
+        })}
       >
         <Flex direction="column" gap="2">
           <Text>Title</Text>
@@ -32,6 +55,7 @@ const CreateTaskPage = () => {
             placeholder="Title"
             {...register("title")}
           />
+          {errors.title && <Text color="red"> {errors.title.message}</Text>}
         </Flex>
         <Flex direction="column" gap="2">
           <Text>Desctiption</Text>
@@ -42,6 +66,9 @@ const CreateTaskPage = () => {
               <SimpleMDE {...field} placeholder="Description" />
             )}
           />
+          {errors.description && (
+            <Text color="red"> {errors.description.message}</Text>
+          )}
         </Flex>
         <Flex direction="column" gap="2" align="start">
           <button
