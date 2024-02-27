89ef017938ec833f3fec99c829363fb2f8927217
David Mutabazi <davidmutabazi@outlook.com>
2024-02-26 21:50:55 -0600

    handle adding new task

--- app/dashboard/tasks/new/page.tsx
+++ app/dashboard/tasks/new/page.tsx
@@ -1,30 +1,58 @@
+"use client";
 import Title from "@/app/components/Title";
-import { Flex, Text } from "@radix-ui/themes";
+import { Flex, Text, Box } from "@radix-ui/themes";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface NewTaskForm {
+  title: string;
+  description: string;
+}
 
 const CreateTaskPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<NewTaskForm>();
   return (
-    <Flex direction="column" gap="4" >
+    <Box>
       <Title title="Create a new task" />
-      <Flex direction="column" gap="2">
-        <Text>Title</Text>
-        <input
-          className="border border-solid py-2 px-4 rounded-[10px]"
-          placeholder="Title"
-        />
-      </Flex>
-      <Flex direction="column" gap="2">
-        <Text>Desctiption</Text>
-        <textarea
-          className="border border-solid py-2 px-4 rounded-[10px]"
-          placeholder="description"
-        />
-      </Flex>
-      <Flex direction="column" gap="2" align="start">
-        <button className="py-2 px-6 bg-[#19948c30] rounded-[5px]">
-          Submit new Task
-        </button>
-      </Flex>
-    </Flex>
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(
+          async (data) => {await axios.post("/api/task", data)
+        router.push("/dashboard/tasks")}
+        )}
+      >
+        <Flex direction="column" gap="2">
+          <Text>Title</Text>
+          <input
+            className="border border-solid py-2 px-4 rounded-[10px]"
+            placeholder="Title"
+            {...register("title")}
+          />
+        </Flex>
+        <Flex direction="column" gap="2">
+          <Text>Desctiption</Text>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE {...field} placeholder="Description" />
+            )}
+          />
+        </Flex>
+        <Flex direction="column" gap="2" align="start">
+          <button
+            className="py-2 px-6 bg-[#19948c30] rounded-[5px]"
+            type="submit"
+          >
+            Submit new Task
+          </button>
+        </Flex>
+      </form>
+    </Box>
   );
 };
 
