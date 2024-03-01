26f730881beeaebb84c5b9037768e3f67ec41452
David Mutabazi <davidmutabazi@outlook.com>
2024-02-29 22:12:00 -0600

    build issue status badge

---
+++ app/components/TaskStatusBadge.tsx
@@ -0,0 +1,19 @@
+import { Status } from "@prisma/client";
+import { Badge } from "@radix-ui/themes";
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "violet" | "green" }
+> = {
+  OPEN: { label: "open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "violet" },
+  CLOSED: { label: "Closed", color: "green" },
+};
+
+const TaskStatusBadge = ({ status }: { status: Status }) => {
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  );
+};
+
+export default TaskStatusBadge;
