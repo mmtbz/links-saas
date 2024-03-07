29c76c1333868b2f59cb3042c69018756681e6b4
David Mutabazi <davidmutabazi@outlook.com>
2024-03-06 22:36:07 -0600

    update hero section

--- app/components/mainPage/HeroSection.tsx
+++ app/components/mainPage/HeroSection.tsx
@@ -1,8 +1,17 @@
-import { Container, Grid, Flex, Box, TextField, Text } from "@radix-ui/themes";
+import {
+  Container,
+  Grid,
+  Flex,
+  Box,
+  TextField,
+  Text,
+  Button,
+} from "@radix-ui/themes";
 import Image from "next/image";
 import WomanImage from "../../../public/images/businesswoman.png";
 import { IoSearch } from "react-icons/io5";
 import Trending from "../../../public/svg/trending.svg";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
@@ -16,19 +25,14 @@ const HeroSection = () => {
             align="center"
             className="px-3 lg:px-0"
           >
-            <div className="font-extrabold text-6xl leading-[5rem] lg:leading-[4rem] mb-6">
-              <div className="relative pt-[3rem]">
-                <div className="uppercase font-bold text-[10rem] text-black lg:text-[15rem] opacity-20 ">
-                  FIND
-                </div>
-                <div className="capitalize font-extrabold lg:font-black text-[3.3rem] lg:text-[4.3rem] leading-[3.3rem] lg:leading-[4.3rem] absolute top-[1rem] lg:top-[-1.5rem] left-[0rem] text-[#F5F5F5]">
-                  Hire the best <br />
-                  candidates in <br />
-                  Africa
-                </div>
+            <div className="mb-6 space-y-4">
+              <div className="capitalize font-extrabold lg:font-black text-[3.3rem] lg:text-[4.3rem] leading-[4.3rem] lg:leading-[5.3rem]  text-[#F5F5F5]">
+                Hire the best <br />
+                candidates in <br />
+                Africa
               </div>
 
-              <p className="font-normal text-base text-[#D5D5D5] leading-6 pt-[4rem] lg:pt-[5rem] max-w-xl">
+              <p className="font-normal text-base text-[#D5D5D5] leading-6  max-w-xl">
                 Find Your New Job Today! New Job Postings Everyday just for you,
                 browse the job you want and apply wherever you want
               </p>
@@ -47,6 +51,7 @@ const HeroSection = () => {
                     priority={false}
                     height={20}
                     width={20}
+                    className="hidden lg:block"
                   />
                 </Flex>
                 <Flex
@@ -60,30 +65,44 @@ const HeroSection = () => {
                     priority={false}
                     height={20}
                     width={20}
+                    className=" hidden lg:block"
                   />
                 </Flex>
-                <Box className="hidden lg:block">
-                  <Flex
-                    align="center"
-                    className="text-white font-normal text-xs uppercase px-4 py-2 bg-white bg-opacity-[0.06] rounded-full gap-1"
-                  >
-                    <Text>Salesforce developer</Text>
-                    <Image
-                      src={Trending}
-                      alt="My Happy SVG"
-                      priority={false}
-                      height={20}
-                      width={20}
-                    />
-                  </Flex>
-                </Box>
+                <Flex
+                  align="center"
+                  className="text-white font-normal text-xs uppercase px-4 py-2 bg-white bg-opacity-[0.06] rounded-full gap-1"
+                >
+                  <Text>Salesforce developer</Text>
+                  <Image
+                    src={Trending}
+                    alt="My Happy SVG"
+                    priority={false}
+                    height={20}
+                    width={20}
+                    className=" hidden lg:block"
+                  />
+                </Flex>
               </Flex>
-              <TextField.Root className="mt-4 " radius="large" size="3">
+              <TextField.Root
+                className="mt-4 outline-none"
+                radius="large"
+                size="3"
+              >
                 <TextField.Slot>
                   <IoSearch size={24} />
                 </TextField.Slot>
                 <TextField.Input placeholder="Search talents…" />
               </TextField.Root>
+
+              <Flex className="text-[#D5D5D5]" justify="between" align="center">
+                <Text>Looking for a job instead? </Text>
+                <Link
+                  href={"/dashboard"}
+                  className="px-4 py-2 bg-orange-400 rounded-[10px] text-black"
+                >
+                  Create account
+                </Link>
+              </Flex>
             </div>
             <Image
               src={WomanImage}
