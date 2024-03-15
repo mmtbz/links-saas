6f71c4dd5986e7545bce5072ab8d983956b2f69e
David Mutabazi <davidmutabazi@outlook.com>
2024-03-14 23:36:47 -0500

    catch errors when updating assignee

--- app/dashboard/tasks/[id]/AssigneeSelect.tsx
+++ app/dashboard/tasks/[id]/AssigneeSelect.tsx
@@ -2,7 +2,6 @@
 import { Task, User } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const AssigneeSelect = ({ task }: { task: Task }) => {
   const {
@@ -20,24 +19,31 @@ const AssigneeSelect = ({ task }: { task: Task }) => {
   if (error) return null;
 
   return (
-    <select
-      id="assigness"
-      className="border border-solid p-1 rounded-custom inline-flex outline-none"
-      onChange={(event) =>
-        axios.patch("/api/task/" + task.id, {
-          assignedToUserId: event.target.value || null,
-        })
-      }
-      defaultValue={task.assignedToUserId || ""}
-    >
-      <option value="-"> Assign User</option>
-      <option value=""> Unassigned</option>
-      {users?.map((user) => (
-        <option value={user.id} key={user.id}>
-          {user.name}
-        </option>
-      ))}
-    </select>
+    <>
+      <select
+        id="assigness"
+        className="border border-solid p-1 rounded-custom inline-flex outline-none"
+        onChange={(event) =>
+          axios
+            .patch("/api/task/" + task.id, {
+              assignedToUserId: event.target.value || null,
+            })
+            .then(() => {
+            })
+            .catch(() => {
+            })
+        }
+        defaultValue={task.assignedToUserId || ""}
+      >
+        <option value="-"> Assign User</option>
+        <option value=""> Unassigned</option>
+        {users?.map((user) => (
+          <option value={user.id} key={user.id}>
+            {user.name}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 
