e8e919ae91d20603b9b92e2be654cc4249a490bb
David Mutabazi <davidmutabazi@outlook.com>
2024-03-18 10:42:48 -0500

    add latest project section

--- app/w/page.tsx
+++ app/w/page.tsx
@@ -1,13 +1,18 @@
 import { Box, Container, Flex, Grid, Separator, Text } from "@radix-ui/themes";
 import Flags from "country-flag-icons/react/3x2";
 import Image from "next/image";
-import { FaClock, FaLocationDot } from "react-icons/fa6";
+import {
+  FaClock,
+  FaLink,
+  FaLocationDot,
+  FaRegCircleDot,
+} from "react-icons/fa6";
 
 const PublicResume = () => {
   return (
     <Container>
       <Grid columns="5">
-        <Box className="col-span-1 bg-gray-100 min-h-screen space-y-3 pt-2">
+        <Box className="col-span-1 bg-gray-50 min-h-screen space-y-3 pt-4">
           <Image
             src="https://randomuser.me/api/portraits/med/men/75.jpg"
             height={100}
@@ -64,7 +69,76 @@ const PublicResume = () => {
             </Box>
           </Flex>
         </Box>
-        <Box className=" col-span-4">hello</Box>
+        <Box className=" col-span-4 pt-4 pl-4">
+          <Flex gap="2" align="center" className="mb-4">
+            <Box>
+              <FaRegCircleDot />
+            </Box>
+            <Text className="text-lg font-semibold">Latest Projects</Text>
+          </Flex>
+          <Flex gap="2" className="pl-6">
+            <Box className="bg-blue-50 p-4 rounded-l-xl space-y-4">
+              <Flex gap="4" >
+                <div className="w-14 h-14 relative">
+                  <Image
+                    src="https://randomuser.me/api/portraits/med/men/75.jpg"
+                    height={100}
+                    width={100}
+                    alt="project cover"
+                    className="absolute inset-0 w-full h-full object-cover rounded-xl"
+                  />
+                </div>
+
+                <Box>
+                  <Text className="font-semibold text-lg" as="p">
+                    Wakka Application
+                  </Text>
+                  <Text>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi nemo atque eos, excepturi impedit quia exercitationem?
+                  </Text>
+                </Box>
+              </Flex>
+              <Box className="inline-flex items-center gap-2 text-blue-600 font-semibold">
+                <Box className="bg-blue-100 p-2 rounded-full">
+                  <FaLink size="20" />
+                </Box>
+
+                <Text>more details</Text>
+              </Box>
+            </Box>
+            <Box className="bg-blue-50 p-4 rounded-r-xl space-y-4">
+              <Flex gap="4" align="start">
+                <div className="w-14 h-14 relative">
+                  <Image
+                    src="https://randomuser.me/api/portraits/med/men/75.jpg"
+                    height={100}
+                    width={100}
+                    alt="project cover"
+                    className="absolute object-cover inset-0 w-full h-full rounded-xl"
+                  />
+                </div>
+
+                <Box>
+                  <Text className="font-semibold text-lg" as="p">
+                    Wakka Application
+                  </Text>
+                  <Text>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi nemo atque eos, excepturi impedit quia exercitationem?
+                  </Text>
+                </Box>
+              </Flex>
+              <Box className="inline-flex items-center gap-2 text-blue-600 font-semibold">
+                <Box className="bg-blue-100 p-2 rounded-full">
+                  <FaLink size="20" />
+                </Box>
+
+                <Text>more details</Text>
+              </Box>
+            </Box>
+          </Flex>
+        </Box>
       </Grid>
     </Container>
   );
