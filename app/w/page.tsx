b72a2f0c0fc07d19423a62f5f6e5fe7dcd6dee97
David Mutabazi <davidmutabazi@outlook.com>
2024-03-19 21:16:23 -0500

    update resume mock

--- app/w/page.tsx
+++ app/w/page.tsx
@@ -4,9 +4,14 @@ import Image from "next/image";
 import {
   FaClock,
   FaLink,
+  FaLinkedinIn,
   FaLocationDot,
   FaRegCircleDot,
 } from "react-icons/fa6";
+import { SiTesla } from "react-icons/si";
+
+import { GoDotFill } from "react-icons/go";
+import { FcGoogle } from "react-icons/fc";
 
 const PublicResume = () => {
   return (
@@ -78,16 +83,18 @@ const PublicResume = () => {
           </Flex>
           <Flex gap="2" className="pl-6">
             <Box className="bg-blue-50 p-4 rounded-l-xl space-y-4">
-              <Flex gap="4" >
-                <div className="w-14 h-14 relative">
-                  <Image
-                    src="https://randomuser.me/api/portraits/med/men/75.jpg"
-                    height={100}
-                    width={100}
-                    alt="project cover"
-                    className="absolute inset-0 w-full h-full object-cover rounded-xl"
-                  />
-                </div>
+              <Flex gap="4">
+                <Box className="min-w-max">
+                  <div className="w-14 h-14 relative">
+                    <Image
+                      src="https://randomuser.me/api/portraits/med/men/75.jpg"
+                      height={100}
+                      width={100}
+                      alt="project cover"
+                      className="absolute inset-0 w-full h-full object-cover rounded-xl"
+                    />
+                  </div>
+                </Box>
 
                 <Box>
                   <Text className="font-semibold text-lg" as="p">
@@ -109,15 +116,17 @@ const PublicResume = () => {
             </Box>
             <Box className="bg-blue-50 p-4 rounded-r-xl space-y-4">
               <Flex gap="4" align="start">
-                <div className="w-14 h-14 relative">
-                  <Image
-                    src="https://randomuser.me/api/portraits/med/men/75.jpg"
-                    height={100}
-                    width={100}
-                    alt="project cover"
-                    className="absolute object-cover inset-0 w-full h-full rounded-xl"
-                  />
-                </div>
+                <Box className=" min-w-max">
+                  <div className="w-14 h-14 relative">
+                    <Image
+                      src="https://randomuser.me/api/portraits/med/men/75.jpg"
+                      height={100}
+                      width={100}
+                      alt="project cover"
+                      className="absolute object-cover inset-0 w-full h-full rounded-xl"
+                    />
+                  </div>
+                </Box>
 
                 <Box>
                   <Text className="font-semibold text-lg" as="p">
@@ -138,6 +147,129 @@ const PublicResume = () => {
               </Box>
             </Box>
           </Flex>
+          <Flex gap="2" align="center" className="my-4">
+            <Box>
+              <FaRegCircleDot />
+            </Box>
+            <Text className="text-lg font-semibold block">Experience</Text>
+          </Flex>
+          <Box className="space-y-4">
+            <Flex className="gap-4">
+              <Box className="pl-6 space-y-3 min-w-max">
+                <Flex
+                  align="center"
+                  className=" text-gray-500 text-sm space-x-2"
+                >
+                  <GoDotFill />
+                  <Text>Jan 2020 - Jan 2022</Text>
+                  <FaLocationDot />
+                  <Text>Cupertino, Ca</Text>
+                </Flex>
+                <Flex className="ml-6" gap="4">
+                  <div className="w-12 h-12 relative bg-blue-600 rounded-xl">
+                    <FaLinkedinIn
+                      className="absolute object-cover inset-0 w-full h-full rounded-xl p-2"
+                      color="white"
+                    />
+                  </div>
+                  <Flex direction="column" gap="1">
+                    <Text className=" font-semibold">Product Designer</Text>
+                    <Text className="font-sm text-gray-500 text-sm">
+                      Linkedin Corp
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Box>
+              <Box>
+                <Text className="text-gray-500">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque id praesentium amet! Eos in dignissimos quos ipsa
+                  recusandae neque, cum commodi facilis eveniet. Illum mollitia
+                  nulla, rem possimus saepe, laudantium odio quibusdam nostrum
+                  esse ipsa perferendis ab laboriosam provident minima
+                  voluptatibus iste eveniet earum molestiae alias placeat.
+                  Facilis, qui consequuntur.
+                </Text>
+              </Box>
+            </Flex>
+            <Flex className="gap-4">
+              <Box className="pl-6 space-y-3 min-w-max">
+                <Flex
+                  align="center"
+                  className=" text-gray-500 text-sm space-x-2"
+                >
+                  <GoDotFill />
+                  <Text>Jan 2020 - Jan 2022</Text>
+                  <FaLocationDot />
+                  <Text>Cupertino, Ca</Text>
+                </Flex>
+                <Flex className="ml-6" gap="4">
+                  <div className="w-12 h-12 relative bg-red-600 rounded-xl">
+                    <SiTesla
+                      className="absolute object-cover inset-0 w-full h-full rounded-xl p-2"
+                      color="white"
+                    />
+                  </div>
+                  <Flex direction="column" gap="1">
+                    <Text className=" font-semibold">Product Designer</Text>
+                    <Text className="font-sm text-gray-500 text-sm">
+                      Linkedin Corp
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Box>
+              <Box>
+                <Text className="text-gray-500">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque id praesentium amet! Eos in dignissimos quos ipsa
+                  recusandae neque, cum commodi facilis eveniet. Illum mollitia
+                  nulla, rem possimus saepe, laudantium odio quibusdam nostrum
+                  esse ipsa perferendis ab laboriosam provident minima
+                  voluptatibus iste eveniet earum molestiae alias placeat.
+                  Facilis, qui consequuntur.
+                </Text>
+              </Box>
+            </Flex>
+            <Flex className="gap-4">
+              <Box className="pl-6 space-y-3 min-w-max">
+                <Flex
+                  align="center"
+                  className=" text-gray-500 text-sm space-x-2"
+                >
+                  <GoDotFill />
+                  <Text>Jan 2020 - Jan 2022</Text>
+                  <FaLocationDot />
+                  <Text>Cupertino, Ca</Text>
+                </Flex>
+                <Flex className="ml-6" gap="4">
+                  <div className="w-12 h-12 relative bg-gray-100 rounded-xl">
+
+                    <FcGoogle
+                      className="absolute object-cover inset-0 w-full h-full rounded-xl p-2"
+                      color="white"
+                    />
+                  </div>
+                  <Flex direction="column" gap="1">
+                    <Text className=" font-semibold">Product Designer</Text>
+                    <Text className="font-sm text-gray-500 text-sm">
+                      Google
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Box>
+              <Box>
+                <Text className="text-gray-500">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloremque id praesentium amet! Eos in dignissimos quos ipsa
+                  recusandae neque, cum commodi facilis eveniet. Illum mollitia
+                  nulla, rem possimus saepe, laudantium odio quibusdam nostrum
+                  esse ipsa perferendis ab laboriosam provident minima
+                  voluptatibus iste eveniet earum molestiae alias placeat.
+                  Facilis, qui consequuntur.
+                </Text>
+              </Box>
+            </Flex>
+          </Box>
         </Box>
       </Grid>
     </Container>
